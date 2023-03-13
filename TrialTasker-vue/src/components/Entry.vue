141389500dffa26df5b860ff6d991b1b1a7f136a
Sebastian Garcia <sfernandogarcia@uts.edu.co>
2023-03-13 11:25:50 -0500

    Se añadieron un resto de funcionalidades

--- TrialTasker-vue/src/components/Entry.vue
+++ TrialTasker-vue/src/components/Entry.vue
@@ -2,7 +2,7 @@
     <div class="entry">
         <div class="card__entry">
             <div class="entry__logo">
-                <Logo />
+                <Logo fill="--brown"/>
             </div>
             <slot></slot>
         </div>
@@ -11,4 +11,27 @@
 
 <script setup>
 import Logo from '@/components/logo/Logo.vue'
+import { onMounted } from 'vue';
+import { useAuthStore } from '@/stores/authStore';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+    authStore.authMessage = null;
+    authStore.authErrors = {};
+});
 </script>
+
+<style scoped>
+.entry {
+  display: grid;
+  place-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background-image: url("@/assets/img/bg-entry.svg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 40px;
+}
+</style>
---
+++ TrialTasker-vue/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="logo">
+        <Logo :fill="'--white'"/>
+    </div>
+    <div class="links" v-if="auth.user">
+        <ButtonLink to="Dashboard" text="Dashboard" v-if="auth.user.is_admin"> <IconDashboard :size="20" stroke-width="2.2" /> </ButtonLink>
+        <ButtonLink to="TypePersonDashboard" text="Tipo de personas" v-if="auth.user.is_admin"> <IconUsers :size="20" stroke-width="2.2" /> </ButtonLink>
+        <ButtonLink to="Home" text="Inicio"> <IconHome :size="20" stroke-width="2.2" /> </ButtonLink>
+    </div>
+    <div class="user">
+        <div class="user__name" v-if="auth.user">
+            <span>{{ auth.user.name }} {{ auth.user.lastname }}</span>
+        </div>
+        <ButtonLink to="Login" text="Salir" @click="auth.handleLogout()"> <IconLogout :size="20" stroke-width="2.2" /> </ButtonLink>
+    </div>
+</template>
+
+<script setup>
+import Logo from '@/components/logo/Logo.vue';
+import { onMounted } from 'vue';
+import { useAuthStore} from '@/stores/authStore';
+import ButtonLink from '../buttons/ButtonLink.vue';
+import { IconHome, IconDashboard, IconLogout, IconUsers } from '@tabler/icons-vue';
+
+const auth = useAuthStore();
+
+onMounted(() => {
+    auth.getUser();
+})
+
+</script>
+
+<style scoped>
+.logo {
+    width: 100%;
+    border-bottom: 2px solid var(--white);
+
+
+}
+.links {
+    width: 100%;
+    padding: 1rem 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+.user {
+    width: 100%;
+    border-top: 2px solid var(--white);
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 0;
+
+}
+.user__name {
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+}
+
+
+
+</style>
