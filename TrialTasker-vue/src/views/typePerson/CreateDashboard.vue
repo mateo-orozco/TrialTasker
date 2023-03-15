882b97b7aec1cd79698d75c3b74955c2c4a24ec5
mateo-orozco <oromateo@hotmail.com>
2023-03-15 17:46:53 -0500

    componente\

--- TrialTasker-vue/src/views/typePerson/CreateDashboard.vue
+++ TrialTasker-vue/src/views/typePerson/CreateDashboard.vue
@@ -1,29 +1,27 @@
 <template>
-  <HeaderAccions title="Crear Tipo de Persona" to="TypePersonDashboard"/>
-    <div class="create-dashboard">
-        <form class="create-dashboard__form" @submit.prevent="typePersons.createTypePerson(form)">
-            <div class="create-dashboard__form__group">
-                <label for="type_person_name">Nombre</label>
-                <input type="text" id="type_person_name" v-model="form.type_person_name" required>
-            </div>
-            <div class="create-dashboard__form__group">
-                <button type="submit">Crear</button>
-            </div>
-        </form>
-    </div>
+    <HeaderAccions title="Crear Tipo de Persona" to="TypePersonDashboard" />
+    <Form :create="typePersons.createTypePerson" :form="form">
+        <FormGroup label="Nombre" :error="typePersons.errors ? typePersons.errors.type_person_name : []">
+            <input type="text" id="type_person_name" v-model="form.type_person_name">
+        </FormGroup>
+    </Form>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import HeaderAccions from '../../components/headers/HeaderAccions.vue';
 import { useTypePersonStore } from '../../stores/typePersonStore';
+import Form from '../../components/forms/Form.vue';
+import FormGroup from '../../components/forms/FormGroup.vue';
+
 
 const typePersons = useTypePersonStore();
+
+typePersons.errorsStore = []
 
 const form = ref({
     type_person_name: ''
 });
-
 
 </script>
 
@@ -33,23 +31,28 @@ const form = ref({
     display: flex;
     justify-content: center;
 }
+
 .create-dashboard__form {
     width: 100%;
     max-width: 500px;
 }
+
 .create-dashboard__form__group {
     margin-bottom: 1rem;
 }
+
 .create-dashboard__form__group label {
     display: block;
     margin-bottom: 0.5rem;
 }
+
 .create-dashboard__form__group input {
     width: 100%;
     padding: 0.5rem;
     border: 1px solid #ccc;
     border-radius: 0.5rem;
 }
+
 .create-dashboard__form__group button {
     width: 100%;
     padding: 0.5rem;
@@ -60,5 +63,4 @@ const form = ref({
     cursor: pointer;
     transition: all 0.3s ease;
 }
-
 </style>
