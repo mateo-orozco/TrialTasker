36b09a56e297d109613f9830dedf1073beed90ef
mateo-orozco <oromateo@hotmail.com>
2023-03-16 16:46:29 -0500

    inicio vista landing 2

---
+++ TrialTasker-vue/src/views/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="navbar">
+        <Logo />>
+    </div>
+</template>
+
+<script setup>
+import Logo from '../components/logo/Logo.vue';
+</script>
+
+<style scoped></style>
