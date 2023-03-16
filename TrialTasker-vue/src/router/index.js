55b0d72ffb70682106cd2eac4dade405ced5f8de
mateo-orozco <oromateo@hotmail.com>
2023-03-16 16:29:35 -0500

    inicio de vista landing

--- TrialTasker-vue/src/router/index.js
+++ TrialTasker-vue/src/router/index.js
@@ -1,63 +1,73 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import axios from 'axios'
-import auth from './auth'
-import typePerson from './typePerson'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import axios from "axios";
+import auth from "./auth";
+import typePerson from "./typePerson";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: 'Login'
+      path: "/",
+      redirect: "Login",
     },
     {
-      path: '/home',
-      name: 'Home',
-      component: HomeView
+      path: "/home",
+      name: "Home",
+      component: HomeView,
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: () => import('../views/DashboardView.vue')
+      path: "/dashboard",
+      name: "Dashboard",
+      component: () => import("../views/DashboardView.vue"),
     },
     {
-      children: [
-        ...auth,
-        ...typePerson,
-      ],
+      children: [...auth, ...typePerson],
     },
-  ]
+  ],
 });
 
 router.beforeEach((to, from, next) => {
-  axios.get('/api/user').then((response) => {
-    if (response.data) {
-      if (to.name === 'Login'|| to.name === 'Register' || to.name === 'ForgotPassword'|| to.name === 'ResetPassword') {
-        if (response.data.is_admin) {
-          next({ name: 'Dashboard' })
-        }else{
-          next({ name: 'Home' })
+  axios
+    .get("/api/user")
+    .then((response) => {
+      if (response.data) {
+        if (
+          to.name === "Login" ||
+          to.name === "Register" ||
+          to.name === "ForgotPassword" ||
+          to.name === "ResetPassword"
+        ) {
+          if (response.data.is_admin) {
+            next({ name: "Dashboard" });
+          } else {
+            next({ name: "Home" });
+          }
+        } else if (to.name === "VerifyEmail") {
+          if (response.data.is_admin) {
+            next({ name: "Dashboard" });
+          } else {
+            next({ name: "Home" });
+          }
+        } else if (to.path.includes("/dashboard/") && !response.data.is_admin) {
+          next({ name: "Home" });
+        } else {
+          next();
         }
-      } else if (to.name === 'VerifyEmail') {
-        if (response.data.is_admin) {
-          next({ name: 'Dashboard' })
-        }else{
-          next({ name: 'Home' })
-        }
-      } else if (to.path.includes('/dashboard/') && !response.data.is_admin) {
-        next({ name: 'Home' })
-      } else {
-        next()
       }
-    }
-  }).catch((error) => {
-    if (to.name === 'Login'|| to.name === 'Register' || to.name === 'ForgotPassword'|| to.name === 'ResetPassword') {
-      next()
-    } else {
-      next({ name: 'Login' })
-    } 
-  })
+    })
+    .catch((error) => {
+      if (
+        to.name === "Login" ||
+        to.name === "Register" ||
+        to.name === "ForgotPassword" ||
+        to.name === "ResetPassword"
+      ) {
+        next();
+      } else {
+        next({ name: "Login" });
+      }
+    });
 });
 
-export default router
+export default router;
