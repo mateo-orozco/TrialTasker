63ac7b1c70120a26eab97d8944111ba343a7018e
Bryan Vanegas <brayanxdvanegas@hotmail.com>
2023-03-21 12:39:57 -0500

    deploy

--- TrialTasker-vue/src/axios.js
+++ TrialTasker-vue/src/axios.js
@@ -3,4 +3,4 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =  "http://localhost:8000/" ;
+axios.defaults.baseURL =  "https://trialtasker-production.up.railway.app/" ;
