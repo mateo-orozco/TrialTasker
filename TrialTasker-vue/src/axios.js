ca0fc7a33ab0abe3a460232c0e5b0f7e540fec24
mateo-orozco <oromateo@hotmail.com>
2023-03-28 18:21:03 -0500

    cors-2

--- TrialTasker-vue/src/axios.js
+++ TrialTasker-vue/src/axios.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 
 
-axios.defaults.withCredentials = false;
-axios.defaults.baseURL =  "https://trialtasker-production.up.railway.app/" ;
-=======
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL =  "https://trialtasker-production.up.railway.app/" ;
->>>>>>> 5ce26b347058271199185a7f002cb21a73692962
