28579ea0f6af9914eae52a252221bbe6e0caacf1
mateo-orozco <70611666+mateo-orozco@users.noreply.github.com>
2023-04-05 15:34:28 -0500

    Update sanctum.php

--- TrialTasker-api/config/sanctum.php
+++ TrialTasker-api/config/sanctum.php
@@ -43,11 +43,11 @@ return [
     | request. You may change the middleware listed below as required.
     |
     */
-    'csrf_cookie' => 'XSRF-TOKEN',
-/* 
+    //'csrf_cookie' => 'XSRF-TOKEN',
+ 
     'middleware' => [
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
     ],
- */
+ 
 ];
